5e0566eb5a0b59b2e8170ebe83792d0552a85273
tronglhse161154 <tronglhse161154@fpt.edu.vn>
2024-10-05 01:46:51 +0700

    add path and user admin logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
+import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Navbar from "./components/layout/navbar/Navbar";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-  Navigate,
-} from "react-router-dom";
+import Footer from "./components/layout/footer/Footer";
 import Home from "./pages/HomePage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -16,47 +12,33 @@ import CategoryPage from "./pages/CategoryPage";
 import SearchModal from "./components/ui/modals/SearchModal";
 import ToasterProvider from "./providers/ToastProvider";
 import OTPConfirmModal from "./components/ui/modals/OTPconfirmModal";
-import { Provider } from "react-redux";
-import store from "./lib/redux/store";
-import Footer from "./components/layout/footer/Footer";
 import AdminPage from "./pages/AdminPage";
-import LayoutWithNavbar from "./LayoutWithNavbar";
-import { useSelector } from "react-redux";
 
 function App() {
   const currentUser = useSelector((state) => state.users.currentUser);
-
   const isAdmin = currentUser?.Role === "Admin";
-
-  console.log(isAdmin)
-
   const location = useLocation();
-  const excludeLayoutPaths = ["/admin"];
-  const shouldExcludeLayout = excludeLayoutPaths.includes(location.pathname);
+  const shouldExcludeLayout = location.pathname.startsWith("/admin");
 
   return (
     <>
-        <SearchModal />
-        <OTPConfirmModal />
-        <ToasterProvider />
-        {!shouldExcludeLayout && <Navbar />}
-        <div className={shouldExcludeLayout ? "no-layout" : "with-layout"}>
-          <Routes>
-            <Route path="/" element={isAdmin ? <Navigate to="/admin" /> : <Home />}/>
-            <Route path="/pokemon" element={""} />
-            <Route path="/elite-exclusive" element={""} />
-            <Route path="/digimon" element={""} />
-            <Route path="/one-piece" element={""} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/viewcard" element={<ViewCard />} />
-            <Route path="/card-detail/:id" element={<DetailPage />} />
-            <Route path="/create-product" element={<CreatePage />} />
-            <Route path="/category/:categoryName" element={<CategoryPage />} />
-            <Route path="/admin" element={<AdminPage />} />
-          </Routes>
-        </div>
-        {!shouldExcludeLayout && <Footer />}
+      <SearchModal />
+      <OTPConfirmModal />
+      <ToasterProvider />
+      {!shouldExcludeLayout && <Navbar />}
+      <div className={shouldExcludeLayout ? "no-layout" : "with-layout"}>
+        <Routes>
+          <Route path="/" element={isAdmin ? <Navigate to="/admin" /> : <Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/viewcard" element={<ViewCard />} />
+          <Route path="/card-detail/:id" element={<DetailPage />} />
+          <Route path="/create-product" element={<CreatePage />} />
+          <Route path="/category/:categoryName" element={<CategoryPage />} />
+          <Route path="/admin/*" element={<AdminPage />} />
+        </Routes>
+      </div>
+      {!shouldExcludeLayout && <Footer />}
     </>
   );
 }
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,36 +1,71 @@
 import React from "react";
-import { Layout, Menu } from "antd";
-import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Menu, Dropdown, Button } from "antd";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Logo from "../components/layout/navbar/Logo";
-import Dashboard from "../components/ui/Dashboard"; // Import your Dashboard component
-import Users from "../components/ui/User"; // Import your Users component
-// Import your Products component
-
+import Dashboard from "../components/ui/Dashboard";
+import Users from "../components/ui/User";
+import { UserOutlined, LogoutOutlined } from "@ant-design/icons";
+import CreateProduct from "./CreateProduct";
+const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
 
 const AdminPage = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Clear any session data or token here
+    localStorage.removeItem("userToken"); // Example: remove token from localStorage
+    navigate("/login"); // Redirect to login page
+  };
+
+  const menu = (
+    <Menu>
+      <Menu.Item key="logout" onClick={handleLogout} icon={<LogoutOutlined />}>
+        Logout
+      </Menu.Item>
+    </Menu>
+  );
+
   return (
     <Layout className="min-h-screen">
-      <Sider collapsible style={{ backgroundColor: 'white' }}> {/* Set Sider background to white */}
-        <div className="flex justify-center items-center p-4">
-          <div className="h-16 w-16"> {/* Increased the size of the logo */}
-            <Logo />
+      <Sider collapsible style={{ backgroundColor: "white" }}>
+        <Link to="/">
+          <div className="flex justify-center items-center p-4">
+            <div className="h-16 w-16">
+              <Logo />
+            </div>
           </div>
-        </div>
-        <Menu theme="light" mode="inline" defaultSelectedKeys={['1']}> {/* Change theme to light */}
-          <Menu.Item key="1"><Link to="/admin/dashboard">Thống kê</Link></Menu.Item>
-          <Menu.Item key="2"><Link to="/admin/users">Quản lý khách hàng</Link></Menu.Item>
-          <Menu.Item key="3"><Link to="/admin/products">Quản lý sản phẩm</Link></Menu.Item>
+        </Link>
+        <Menu theme="light" mode="inline" defaultSelectedKeys={["1"]}>
+          <Menu.Item key="1">
+            <Link to="/admin/dashboard">Thống kê</Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link to="/admin/users">Quản lý khách hàng</Link>
+          </Menu.Item>
+          <SubMenu key="sub1" title="Quản lý sản phẩm">
+            <Menu.Item key="3">
+              <Link to="/admin/products">Danh sách sản phẩm</Link>
+            </Menu.Item>
+            <Menu.Item key="4">
+              <Link to="/admin/create">Thêm sản phẩm</Link>
+            </Menu.Item>
+          </SubMenu>
         </Menu>
       </Sider>
       <Layout className="site-layout">
-        <Header className="site-layout-background p-0 bg-white">
-          <h1 className="text-center"></h1>
+        <Header className="site-layout-background p-0 bg-white flex justify-end items-center">
+          <Dropdown overlay={menu} placement="bottomRight" trigger={['click']}>
+            <div className="flex justify-end text-3xl py-2 px-6">
+            <UserOutlined/>
+            </div>
+          </Dropdown>
         </Header>
         <Content className="m-4">
           <Routes>
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/users" element={<Users />} />
+            <Route path="/create" element={<CreateProduct />} />
             <Route path="/" element={<Dashboard />} />
           </Routes>
         </Content>
